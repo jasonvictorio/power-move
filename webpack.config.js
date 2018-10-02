4ab042267ddd86100936b87c7a4861afa51b4075
jasonvictorio <jgvictorio@gmail.com>
2018-10-02 21:48:23 +0800

    add webpack-dev-server and browser-sync

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     path: `${__dirname}/dist`,
     filename: 'main.js',
   },
   plugins: [
     new HtmlWebpackPlugin(),
+    new BrowserSyncPlugin(
+      {
+        host: 'localhost',
+        port: 3000,
+        proxy: 'http://localhost:8080/',
+        files: ['src/*'],
+      },
+      {
+        reload: false
+      }
+    )
   ],
 }
