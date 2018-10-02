8233f5b92f88abf282845e540b5eadbc5b01d910
Jason Victorio <jgvictorio@gmail.com>
2018-10-03 01:08:13 +0800

    move 'mode' to script flags

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'development',
   output: {
     path: `${__dirname}/dist`,
     filename: 'main.js',
