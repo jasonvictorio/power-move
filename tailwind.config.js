8e1aadb106251d52aacd5f37f79c5d7fac6cf60c
Jason Victorio <jgvictorio@gmail.com>
2018-10-12 21:11:19 +0800

    update font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -250,15 +250,17 @@ module.exports = {
   */
 
   textSizes: {
-    'xs': '.75rem',     // 12px
-    'sm': '.875rem',    // 14px
-    'base': '1rem',     // 16px
-    'lg': '1.125rem',   // 18px
-    'xl': '1.25rem',    // 20px
-    '2xl': '1.5rem',    // 24px
-    '3xl': '1.875rem',  // 30px
-    '4xl': '2.25rem',   // 36px
-    '5xl': '3rem',      // 48px
+    'base': '1.6rem',
+
+    '1': '1.6rem',
+    '2': '2.4rem',
+    '3': '3.2rem',
+    '4': '4.0rem',
+    '5': '4.8rem',
+    '6': '5.6rem',
+    '7': '6.4rem',
+    '8': '7.2rem',
+    '9': '8.0rem',
   },
 
 
