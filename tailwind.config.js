d61a62c52b1ca2a379063c95eeb99face5ac8844
Jason Victorio <jgvictorio@gmail.com>
2020-07-04 05:24:34 +1200

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  theme: {
+    screens: {
+      xl: { max: '1366px' },
+      lg: { max: '1024px' },
+      md: { max: '768px' },
+      sm: { max: '480px' },
+      xs: { max: '361px' },
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
