012a39611b53a6ac8efd8a5546af3abef842e5a5
Jason Victorio <jgvictorio@gmail.com>
2020-07-12 00:01:09 +1200

    responsive

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.scss'
+
+const menuToggle = document.getElementById('menu-toggle')
+const menu = document.getElementById('menu')
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('is-active')
+  menu.classList.toggle('md:-translate-x-full')
+})
